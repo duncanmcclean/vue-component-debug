e55b2412425e8875f206e1190fe794fc7e7a41ad
Duncan McClean <duncan@duncanmcclean.com>
2025-07-03 12:04:11 +0100

    Early return.

--- src/mixins/componentDebug.js
+++ src/mixins/componentDebug.js
@@ -1,40 +1,44 @@
 export function createComponentDebugMixin() {
     return {
         mounted() {
-            if (process.env.NODE_ENV === 'development') {
-                const component = this.$options.__file || this.$options.__name || 'Anonymous';
-
-                const startComment = document.createComment(` Start component: ${component} `);
-                this.$el.parentNode?.insertBefore(startComment, this.$el);
-
-                const endComment = document.createComment(` End component: ${component} `);
-                this.$el.parentNode?.insertBefore(endComment, this.$el.nextSibling);
+            if (process.env.NODE_ENV !== 'development') {
+                return;
             }
+
+            const component = this.$options.__file || this.$options.__name || 'Anonymous';
+
+            const startComment = document.createComment(` Start component: ${component} `);
+            this.$el.parentNode?.insertBefore(startComment, this.$el);
+
+            const endComment = document.createComment(` End component: ${component} `);
+            this.$el.parentNode?.insertBefore(endComment, this.$el.nextSibling);
         },
         beforeUnmount() {
-            if (process.env.NODE_ENV === 'development') {
-                // Clean up comments when components are destroyed.
-                const component = this.$options.__file || this.$options.__name || 'Anonymous';
-                const parent = this.$el.parentNode;
+            if (process.env.NODE_ENV !== 'development') {
+                return;
+            }
 
-                if (parent) {
-                    let node = this.$el.previousSibling;
-                    while (node && node.nodeType === Node.COMMENT_NODE) {
-                        if (node.nodeValue === ` Start component: ${component} `) {
-                            parent.removeChild(node);
-                            break;
-                        }
-                        node = node.previousSibling;
-                    }
+            // Clean up comments when components are destroyed.
+            const component = this.$options.__file || this.$options.__name || 'Anonymous';
+            const parent = this.$el.parentNode;
 
-                    node = this.$el.nextSibling;
-                    while (node && node.nodeType === Node.COMMENT_NODE) {
-                        if (node.nodeValue === ` End component: ${component} `) {
-                            parent.removeChild(node);
-                            break;
-                        }
-                        node = node.nextSibling;
+            if (parent) {
+                let node = this.$el.previousSibling;
+                while (node && node.nodeType === Node.COMMENT_NODE) {
+                    if (node.nodeValue === ` Start component: ${component} `) {
+                        parent.removeChild(node);
+                        break;
                     }
+                    node = node.previousSibling;
+                }
+
+                node = this.$el.nextSibling;
+                while (node && node.nodeType === Node.COMMENT_NODE) {
+                    if (node.nodeValue === ` End component: ${component} `) {
+                        parent.removeChild(node);
+                        break;
+                    }
+                    node = node.nextSibling;
                 }
             }
         },
